b358d7d260a5baf65e35d3b3ab34850fc43d70f4
paddlehands88 <stefancorlett@hotmail.com>
2024-03-22 14:39:22 +0000

    Initial upload of completed project post fixing bug

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+import type { PrismaClient } from '@prisma/client';
+import type { MongoClient } from 'mongodb';
+
+
+declare global {
+  namespace globalThis {
+    var prismadb: PrismaClient
+  }
+}
